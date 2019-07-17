84e7159efa94b5be93707a147a8e687ab74b517c
Roque Santos <roque.santos@cronapp.io>
2019-07-17 10:17:16 -0300

    correção de bug no logout

--- ui/cockpit/client/scripts/config.js
+++ ui/cockpit/client/scripts/config.js
@@ -2,12 +2,5 @@ window.camCockpitConf = {
   'locales':{
     'availableLocales': ['en', 'pt-BR'],
     'fallbackLocale': 'pt-BR'
-  },
-  customScripts: {
-    ngDeps: ['cronLogout'],
-    deps: ['cronLogout'],
-    paths: {
-      'cronLogout': '../../../common/scripts/module/services/logout'
-    }
   }
 };
--- ui/tasklist/client/scripts/config/config.js
+++ ui/tasklist/client/scripts/config/config.js
@@ -3,13 +3,6 @@ window.camTasklistConf = {
     'availableLocales': ['en', 'pt-BR'],
     'fallbackLocale': 'pt-BR'
   },
-  customScripts: {
-    ngDeps: ['cronLogout'],
-    deps: ['cronLogout'],
-    paths: {
-      'cronLogout': '../../../../common/scripts/module/services/logout'
-    }
-  },
   'shortcuts': {
     'claimTask': {
       'key': 'ctrl+alt+c',
