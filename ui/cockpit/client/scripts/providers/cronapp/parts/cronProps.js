d30fad14fad0270a5b63b43cc4e9a11ffc1a14fa
Roque Santos <roque.santos@cronapp.io>
2019-07-24 11:51:14 -0300

    rollback no props

--- ui/cockpit/client/scripts/providers/cronapp/parts/cronProps.js
+++ ui/cockpit/client/scripts/providers/cronapp/parts/cronProps.js
@@ -4,46 +4,30 @@ var entryFactory = require('bpmn-js-properties-panel/lib/factory/EntryFactory'),
 var ModelUtil = require('bpmn-js/lib/util/ModelUtil'), is = ModelUtil.is,
   getBusinessObject = ModelUtil.getBusinessObject;
 
-module.exports = function(group, element, Uri, $http) {
+module.exports = function(group, element, bpmnFactory, translate) {
 
-  var blocklyList = [];
-
-  $http.get(Uri.appUri('engine://engine/:engine/blockly')).success(function(data) {
-    blocklyList = data;
-
-    var elementBlocklyEntry = entryFactory.comboBox({
-      id: 'blockly-select',
-      label: 'Blockly',
-      selectOptions: blocklyList,
-      modelProperty: 'name',
-      emptyParameter: true
-    });
-
-    var elementMethodsEntry = entryFactory.comboBox({
-      id: 'method-select',
-      label: 'Methods',
-      selectOptions: blocklyList,
-      modelProperty: 'name',
-      emptyParameter: true
-    });
-
-    elementBlocklyEntry.get = getValue(getBusinessObject(element));
-
-    elementBlocklyEntry.set = setValue(getBusinessObject(element));
-
-    elementMethodsEntry.get = getValue(getBusinessObject(element));
-
-    elementMethodsEntry.set = setValue(getBusinessObject(element));
-
-    if (is(element, 'bpmn:ServiceTask')) {
-      group.entries.push(elementBlocklyEntry);
-      group.entries.push(elementMethodsEntry);
-    }
-  });
+  var blocklyList = [{
+    name: 'blocoTeste',
+    value: 'Bloco Teste',
+    methods: [{name: 'executar', value: 'Executar', parameters: ['abc', 'teste']}, {
+      name: 'rodar',
+      value: 'Rodar',
+      parameters: ['abcd', 'teste2']
+    }]
+  }, {
+    name: 'blocoTeste2',
+    value: 'Bloco Teste 2',
+    methods: [{name: 'executar2', value: 'Executar2', parameters: ['abc2', 'teste2']}, {
+      name: 'rodar2',
+      value: 'Rodar2',
+      parameters: ['abcd', 'teste2']
+    }]
+  }];
 
   var getMethods = function(blocklys) {
 
   };
+
 
   var getValue = function(businessObject) {
     return function(element) {
@@ -59,4 +43,32 @@ module.exports = function(group, element, Uri, $http) {
       return {};
     }
   };
+  var elementBlocklyEntry = entryFactory.comboBox({
+    id: 'blockly-select',
+    label: 'Blockly',
+    selectOptions: blocklyList,
+    modelProperty: 'name',
+    emptyParameter: true
+  });
+
+  var elementMethodsEntry = entryFactory.comboBox({
+    id: 'method-select',
+    label: 'Methods',
+    selectOptions: blocklyList,
+    modelProperty: 'name',
+    emptyParameter: true
+  })
+
+  elementBlocklyEntry.get = getValue(getBusinessObject(element));
+
+  elementBlocklyEntry.set = setValue(getBusinessObject(element));
+
+  elementMethodsEntry.get = getValue(getBusinessObject(element));
+
+  elementMethodsEntry.set = setValue(getBusinessObject(element));
+
+  if(is(element, 'bpmn:ServiceTask')) {
+    group.entries.push(elementBlocklyEntry);
+    group.entries.push(elementMethodsEntry);
+  }
 };
